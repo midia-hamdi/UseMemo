099cd61016f538406ac46a777ebdc85631bf2c15
midia-hamdi <midiahamdi367@gmail.com>
2023-12-05 18:05:02 +0330

    Usememo

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+
+
+function wordCounte(word) {
+    let i = 0;
+    do {
+        i++
+    } while (i <= 999999999)
+
+    return word.length;
+}
 
 export default function Counter() {
-  return (
-    <div>Counter</div>
-  )
+    const [counter1, setCounter1] = useState(0)
+    const [counter2, setCounter2] = useState(0)
+
+    //const randomNumber = useMemo(() => Math.random() * counter1, [counter1])
+
+
+    //const randomNumber = Math.random() * counter1;
+
+    const count = useMemo(() => wordCounte('js'), [])
+
+    return (
+        <div>
+            <p>Word Count: {count}</p>
+            {/* <p>Random number is: {randomNumber}</p> */}
+            <p>Click count1: {counter1}</p>
+            <p>Click count2: {counter2}</p>
+            <div>
+                <button onClick={() => setCounter1(c => c + 1)}>Increament1</button>
+                <button onClick={() => setCounter1(c => c - 1)}>Decrease1</button>
+            </div>
+            <div>
+                <button onClick={() => setCounter2(c => c + 1)}>Increament2</button>
+                <button onClick={() => setCounter2(c => c - 1)}>Decrease2</button>
+            </div>
+        </div>
+    )
 }
+
